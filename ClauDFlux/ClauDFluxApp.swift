a550a06e4e477167ccec6c6affcad0294fc66458
claudia1402 <claudiafiorentino2002@gmail.com>
2022-08-08 16:11:09 -0300

    Initial Commit

---
+++ ClauDFlux/ClauDFluxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClauDFluxApp.swift
+//  ClauDFlux
+//
+//  Created by Claudia Fiorentino on 08/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ClauDFluxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
